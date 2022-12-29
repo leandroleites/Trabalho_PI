5aa0f77d97de0319c9ae40e3de99e1276786afd3
Leandro Ferreira <leandroleitesferreira@hotmail.com>
2022-12-29 18:07:56 +0000

    Atualizadas as 3 funções de carregar

--- Trabalho PI/trabalhopi.c
+++ Trabalho PI/trabalhopi.c
@@ -39,7 +39,7 @@ void carregarparticipantes()
 
 } 
 
-typedef struct {
+struct atividades {
     int id;
     char date[12];
     char time[6];
@@ -47,25 +47,32 @@ typedef struct {
     int duration;
     int distance;
     char units[2];
-} Activity;
+} ;
 
-void carregaratividades(const char* filename, Activity* activities, int* numActivities) {
-    FILE* file = fopen("C:/Users/leand/Documents/Trabalho_PI/Trabalho PI/dados/2Informacao_de_cada_atividade_realizada_por_cada_praticante.txt", "r");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file: %s\n", filename);
-        exit(1);
-    }
+void carregaratividades() {
+    int numActivities;
+    FILE *fp;
+    fp = fopen("C:/Users/leand/Documents/Trabalho_PI/Trabalho PI/dados/2Informacao_de_cada_atividade_realizada_por_cada_praticante.txt", "r");
+     if(fp != NULL) {
+         printf("Atividades carregadas\n");
+         struct atividades atividades[100];
+         char line[100];
+         while (fgets(line, sizeof(line), fp) != NULL)
+         {
+            sscanf(line, "%d;%[^;];%[^;];%[^;];%d;%d;%s", &atividades[numActivities].id, atividades[numActivities].date, atividades[numActivities].time, atividades[numActivities].activity, &atividades[numActivities].duration, &atividades[numActivities].distance, atividades[numActivities].units);
+           (numActivities)++;
+         }
+         fclose(fp);
+     }else 
+      {
+         printf("Erro ao abrir o ficheiro.\n");
+      }
+         
 
-    char line[100];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        sscanf(line, "%d;%[^;];%[^;];%[^;];%d;%d;%s", &activities[*numActivities].id, activities[*numActivities].date, activities[*numActivities].time, activities[*numActivities].activity, &activities[*numActivities].duration, &activities[*numActivities].distance, activities[*numActivities].units);
-        (*numActivities)++;
-    }
-
-    fclose(file);
+    fclose(fp);
 }
 
-typedef struct {
+ struct plano {
     int id;
     char dateinicio[12];
     char timeinicio[6];
@@ -74,22 +81,30 @@ typedef struct {
     char activity[20];
     int distanceatingir;
     char units[2];
-} planoatividade;
+};
 
-void carregarplano(const char* filename, planoatividade* plano, int* numplano) {
-    FILE* file = fopen("C:/Users/leand/Documents/Trabalho_PI/Trabalho PI/dados/3Dados_dos_planos_de_atividades.txt", "r");
-    if (file == NULL) {
-        fprintf(stderr, "Error opening file: %s\n", filename);
-        exit(1);
+void carregarplano() {
+    int numplano;
+    FILE *fp;
+    fp = fopen("C:/Users/leand/Documents/Trabalho_PI/Trabalho PI/dados/3Dados_dos_planos_de_atividades.txt", "r");
+    if (fp != NULL) {
+         printf("Planos carregados\n");
+        struct plano plano[100];
+         char line[100];
+         while (fgets(line, sizeof(line), fp) != NULL) {
+        sscanf(line, "%d;%[^;];%[^;];%[^;];%d;%d;%s", &plano[numplano].id, plano[numplano].dateinicio, plano[numplano].timeinicio, plano[numplano].datefim, plano[numplano].timefim, plano[numplano].activity, plano[numplano].units);
+        (numplano)++;
+    }
+        fclose(fp);
+    }else 
+    {
+         printf("Erro ao abrir o ficheiro.\n");
     }
 
-    char line[100];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        sscanf(line, "%d;%[^;];%[^;];%[^;];%d;%d;%s", &plano[*numplano].id, plano[*numplano].dateinicio, plano[*numplano].timeinicio, plano[*numplano].datefim, plano[*numplano].timefim, plano[*numplano].activity, plano[*numplano].units);
-        (*numplano)++;
-    }
+    
+  
 
-    fclose(file);
+    
 }
 
 
@@ -118,9 +133,14 @@ void opcoesgerais()
 int main() 
 {
    carregarparticipantes();
+   carregaratividades();
+   carregarplano();
    opcoesgerais();
+
    int op;
    struct clientes cliente;
+   struct plano plano;
+   struct atividades atividades;
    int i;
   
    printf("Insira a opcao que deseja: ");
